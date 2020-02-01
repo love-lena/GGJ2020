5e93e2301ad195ebf78a8638fe7861d5d62376f1
C-Dorr <c.dorr.97@gmail.com>
2020-02-01 12:06:54 -0800

    Create SuckingEnemy.cs
    Health manager must be drag-dropped on

---
+++ Assets/Scripts/SuckingEnemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuckingEnemy : MonoBehaviour
+{
+    public HealthManager healthManager;
+    private bool isSucking;
+
+    void Start()
+    {
+        isSucking = false;
+    }
+
+    void Update() { 
+}
+
+    //Goes on player
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag.Equals("enemy"))
+        {
+            isSucking = true;
+            healthManager.Sucking(collider.gameObjects);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag.Equals("enemy"))
+        {
+            isSucking = false;
+        }
+    }
+}
