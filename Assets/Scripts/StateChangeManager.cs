567058b71191aac2fc40c37b3fab845dae8817a7
C-Dorr <c.dorr.97@gmail.com>
2020-02-01 11:41:44 -0800

    Create StateChangeManager.cs

---
+++ Assets/Scripts/StateChangeManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateChangeManager : MonoBehaviour
+{
+    private HealthManager healthManager;
+    private string gameState;
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthManager = this.gameObject.GetComponent<HealthManager>();
+        gameState = "starting";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while (gameState.Equals("starting"))
+        {
+            if(!Input.GetKeyDown(KeyCode.Escape) && Input.anyKey)
+            {
+                gameManager.UpdateState("playing");
+            }
+
+            if(Input.GetKeyDown(KeyCode.Escape))
+            {
+                Application.Exit();
+            }
+        }
+
+        while (gameState.Equals("playing"))
+        {
+            if(healthManager.GetHealth() >= 0.0)
+            {
+                gameState = "failed";
+            }
+        }
+
+        while (gameState.Equals("failed"))
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                gameState = "playing";
+            }
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                gameState = "starting";
+            }
+        }
+    }
+}
