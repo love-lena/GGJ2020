1fd9f7b2b6829f0e03a6e09bc5eb5afb9859d035
C-Dorr <c.dorr.97@gmail.com>
2020-02-01 11:23:24 -0800

    Update ClickAttackMovement.cs
    Changes to Time.deltaTime functionality
    
    Commented out reference to PlayerController

--- Assets/Scripts/ClickAttackMovement.cs
+++ Assets/Scripts/ClickAttackMovement.cs
@@ -4,21 +4,24 @@ using UnityEngine;
 
 public class ClickAttackMovement : MonoBehaviour
 {
-    bool canAttack;
+    bool canAttack; 
     GameObject player;
     Transform playerTrans;
+    private float speed;
     // Start is called before the first frame update
     void Start()
     {
         canAttack = true;
         player = this.gameObject;
         playerTrans = player.GetComponent<Transform>();
+
+        speed = 5.0F;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseDown(0) && canAttack)
+        if(Input.GetMouseButtonDown(0) && canAttack)
         {
             StartCoroutine("Attack");
         }
@@ -27,18 +30,21 @@ public class ClickAttackMovement : MonoBehaviour
     IEnumerator Attack()
     {
         canAttack = false;
-        player.GetComponent<PlayerController>().SetState("attacking");
-        Vector2 currentPos = (Vector2)playerTrans.position;
+        //player.GetComponent<PlayerController>().SetState("attacking");
+        Vector3 currentPos = (Vector2)playerTrans.position;
 
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 movementVec = mousePos - currentPos;
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 movementVec = (mousePos - currentPos).normalized;
 
-        for (float ft = 0.0F; ft <= 1.0F; ft += 0.1f)
-        {
-            player.transform.position = currentPos + movementVec * ft;
-            yield return new WaitForSeconds(.01F);
-        }
+        //for (float ft = 0.0F; ft <= 1.0F; ft += 0.1f)
+        //{
+        //    player.transform.position = currentPos + movementVec * ft;
+        //    yield return new WaitForSeconds(.01F);
+        //}
+
+        playerTrans.position += movementVec * Time.deltaTime * speed;
 
         canAttack = true;
+        yield return null;
     }
 }
