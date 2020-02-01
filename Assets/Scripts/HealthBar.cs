01cfd90374edda9b4ffe46d94069da8a574c48e1
Sean Slater <sean@slaterfam.com>
2020-02-01 10:17:12 -0800

    Sean Init

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+	float health;
+	float maxHealth;
+	GameObject gm;
+	HealthManager hs;
+	GameObject mask;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		gm = GameObject.FindWithTag("GameManager");
+		mask = this.gameObject.transform.Find("HealthBarMask").gameObject;
+		hs = gm.GetComponent<HealthManager>();
+		health = hs.GetHealth();
+		maxHealth = hs.GetMaxHealth();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		health = hs.GetHealth();
+		mask.transform.localScale = new Vector3(health/maxHealth, 1, 1);
+	}
+}
---
+++ Assets/Scripts/HealthManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+	float maxHealth;
+	float health;
+	float lose;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		maxHealth = health = 20f;
+		lose = 1f;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		health -= lose * Time.deltaTime;
+	}
+	
+	public float GetHealth() {
+		return health;
+	}
+	
+	public float GetMaxHealth() {
+		return maxHealth;
+	}
+}
