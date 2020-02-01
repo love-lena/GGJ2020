0b1f4cde25e060966d1d70d2dec37ad9b38b3adb
C-Dorr <c.dorr.97@gmail.com>
2020-02-01 11:05:36 -0800

    Create CameraFollowsPlayer.cs

---
+++ Assets/Scripts/CameraFollowsPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowsPlayer : MonoBehaviour
+{
+    public Camera mainCam;
+    private Transform playerTrans;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTrans = this.gameObject.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        mainCam.transform.position = playerTrans.transform.position;
+    }
+}
